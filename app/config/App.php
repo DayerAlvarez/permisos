e1ba6a0b495ffe5c42003267e543c1cdf55c8801
DayerAlvarez <dayeralvarez2004@gmail.com>
2024-10-17 09:51:17 -0500

    hola

---
+++ app/config/App.php
@@ -0,0 +1,12 @@
+<?php
+
+//Constantes globales
+const SERVERURL = "http://localhost/permisos/";
+const COMPANY = "SENATI";
+const VERSION = "1.0";
+
+//Configuracion horaria
+date_default_timezone_set("America/Lima");
+
+//Recursos - HELPERS...
+
